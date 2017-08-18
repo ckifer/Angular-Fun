26efa908959215f8aadf92cd304585ac55992e1a
Coltin Kifer <itin.c.kifer@setonhill.edu>
2017-08-18 14:50:37 -0400

    add hero-detail component using cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HeroDetailComponent } from './hero-detail.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 import { AppComponent } from './app.component';
 
