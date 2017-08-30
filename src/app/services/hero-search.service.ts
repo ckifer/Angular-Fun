e3a06c28158cfd330b3f2b8ac4f8043cc7b0ecdd
Coltin Kifer <itin.c.kifer@setonhill.edu>
2017-08-30 10:02:43 -0400

    fix spaces in search. Tutorial Done!

--- src/app/services/hero-search.service.ts
+++ src/app/services/hero-search.service.ts
@@ -13,7 +13,7 @@ export class HeroSearchService {
 
   search(term: string): Observable<Hero[]> {
     return this.http
-      .get(`api/heroes/?name = ${term}`)
+      .get(`api/heroes/?name=${term}`)
       .map(response => response.json().data as Hero[]);
   }
 
